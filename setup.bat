d3f109740192d053aa6f2cd0a117a4f6a84c9976
blighli <blighli@gmail.com>
2015-04-23 21:48:37 +0800

    fix server config

--- setup.bat
+++ setup.bat
@@ -1,9 +1,10 @@
 @echo off
 if not exist venv (
     virtualenv venv
-    venv\Scripts\pip install -r requirements.txt
-    if exist migrations (
-        venv\Scripts\python manage.py db upgrade
-        venv\Scripts\python manage.py setup
-    )
+)
+
+venv\Scripts\pip install -r requirements.txt
+
+if exist migrations (
+    venv\Scripts\python manage.py db upgrade
 )
